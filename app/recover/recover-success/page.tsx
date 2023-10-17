faffb83bb5b8241995064a4c64f8eb730ed4ebd6
AnhTuTran12 <tutrananh.266@gmail.com>
2023-10-17 20:59:02 +0700

    #1 - ui recover password

---
+++ app/recover/recover-success/page.tsx
@@ -0,0 +1,88 @@
+export default function login() {
+  return (
+    <div className="fixed top-0 bottom-0 right-0 left-0 z-10 bg-white">
+      <div className="flex flex-row max-w-xxs mx-auto lg:items-center md:items-center h-full lg:m-0 lg:items-stretch truncate lg:h-screen lg:max-w-7xl md:max-w-3xl md:m-auto">
+        <section className="hidden lg:w-[450px] lg:grow-0 lg:block  ">
+          <div className="lg:h-full lg:flex lg:flex-col lg:justify-between">
+            <video
+              playsInline
+              className="lg:w-full lg:h-full lg:object-cover lg:overflow-clip"
+              src="https://cdn.dribbble.com/uploads/48226/original/b8bd4e4273cceae2889d9d259b04f732.mp4?1689028949"
+              autoPlay
+              loop
+              muted
+            ></video>
+          </div>
+        </section>
+        <section className="flex flex-col flex-1 overflow-auto w-full  ">
+          <div className=" flex justify-center lg:items-center md:items-center grow m-0 p-0 pt-5 mt-20 ">
+            <div className="lg:max-w-[440px] max-w-[360px] max-h-[422px]  ">
+              <div className="max-w-[327px] max-h-[204.12px]  mx-auto">
+                <div className="w-full h-full left-0 relative ">
+                  <svg
+                    width="312"
+                    height="242"
+                    viewBox="0 0 312 242"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M26.053 220.495C26.053 220.495 -3.77393 201.006 0.40061 157.713C5.02242 109.775 23.992 104.536 38.3486 97.4982C54.6433 89.5085 68.5877 88.1289 87.2766 48.6434C111.947 -3.48593 156.341 0.0504746 173.144 0.0504746C230.202 0.0504746 229.518 56.3798 257.065 84.4179C282.998 110.806 339.67 168.008 275.482 218.679C242.059 245.058 79.4976 253.45 26.053 220.495Z"
+                      fill="#C0E5F0"
+                    />
+                    <path
+                      d="M264.309 115.953C290.648 115.953 312 94.6007 312 68.2616C312 41.9225 290.648 20.5704 264.309 20.5704C237.97 20.5704 216.618 41.9225 216.618 68.2616C216.618 94.6007 237.97 115.953 264.309 115.953Z"
+                      fill="#32B768"
+                    />
+                    <path
+                      d="M257.416 87.6347L239.399 73.2091C237.516 71.6938 236.92 68.9961 238.182 66.9378C239.706 64.4416 243.008 63.9248 245.215 65.6941L261.594 78.8059L282.676 52.9239C284.331 50.8919 287.327 50.5853 289.359 52.2407C291.391 53.8961 291.697 56.8916 290.042 58.9236L267.523 86.5573C265.018 89.6316 260.508 90.1134 257.416 87.6347Z"
+                      fill="white"
+                    />
+                    <path
+                      d="M30.3892 57.769C29.9931 56.2075 27.0072 55.5296 23.0007 55.8571C22.2162 55.918 21.4849 55.4839 21.1727 54.7679C19.5731 51.0813 17.5546 48.7733 15.9931 49.1618C14.4317 49.5579 13.7537 52.5437 14.0813 56.5502C14.1422 57.3348 13.708 58.066 12.9921 58.3783C9.30546 59.9779 6.99752 61.9964 7.38599 63.5578C7.78207 65.1193 10.7679 65.7972 14.7744 65.4697C15.559 65.4087 16.2902 65.8429 16.6025 66.5589C18.202 70.2455 20.2205 72.5534 21.782 72.165C23.3435 71.7689 24.0214 68.7831 23.6939 64.7765C23.6329 63.992 24.0671 63.2608 24.7831 62.9485C28.4697 61.3413 30.7852 59.3304 30.3892 57.769Z"
+                      fill="#32B768"
+                    />
+                    <path
+                      d="M279.982 156.835C280.241 155.251 277.765 153.431 273.964 152.143C273.225 151.892 272.723 151.199 272.723 150.414C272.715 146.4 271.793 143.475 270.201 143.209C268.617 142.95 266.797 145.425 265.509 149.226C265.258 149.965 264.565 150.468 263.78 150.468C259.766 150.475 256.841 151.397 256.575 152.989C256.316 154.573 258.791 156.394 262.592 157.681C263.331 157.932 263.834 158.625 263.834 159.41C263.841 163.424 264.763 166.349 266.355 166.616C267.939 166.875 269.76 164.399 271.047 160.598C271.298 159.859 271.991 159.357 272.776 159.357C276.798 159.349 279.723 158.427 279.982 156.835Z"
+                      fill="#EDB82C"
+                    />
+                    <path
+                      d="M13.289 207.671C18.5085 207.671 22.7397 203.44 22.7397 198.22C22.7397 193.001 18.5085 188.77 13.289 188.77C8.06958 188.77 3.83838 193.001 3.83838 198.22C3.83838 203.44 8.06958 207.671 13.289 207.671Z"
+                      fill="#32B768"
+                    />
+                  </svg>
+
+                  <img
+                    className="max-w-[204px] max-h-44 top-[50px] left-12 absolute"
+                    src="../images/recover/relaxed 1.png"
+                  />
+                </div>
+              </div>
+              <div className="mt-14">
+                <div className="  text-gray-700 text-xl font-bold leading-9 text-center	">
+                  Thành công
+                </div>
+                <div className="text-gray-400 text-base font-medium leading-[21px] text-center">
+                  Vui lòng kiểm tra email của bạn
+                </div>
+                <div className="text-gray-400 text-base font-medium leading-[21px] text-center">
+                  <span className="text-gray-400 ">Chưa nhận được email?</span>{" "}
+                  <a href="" className="text-blue-600 ">
+                    Gửi lại
+                  </a>
+                </div>
+              </div>
+              <div className="lg:max-w-[440px] max-w-[360px] mt-3">
+                <div>
+                  <button className="rounded-lg bg-primary text-white font-medium  w-[360px] md:text-base mt-5 text-sm h-14  hover:bg-blue-500 lg:w-[440px]">
+                    Quay lại
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
